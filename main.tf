7fc4cd7098b4e42556372804492baee1cf08c944
David Pugh <davidp@TERRA-DSC-01.dsc.dmz>
2019-07-22 15:08:42 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  version = "~> 2.18"
+  region = "eu-west-2"
+}
+
+
+variable "server_port"{
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value  = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-7ad7c21e"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+               echo "hello, World" > index.html
+               nohup busybox httpd -f -p ${var.server_port} &
+               EOF
+  
+  tags={
+    Name= "terraform-example-dp"
+    }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+}
